527b45d6d553f3e0cc3f66f74eb1cb7c9b9c5b51
AjayKumarNagula <96443142+AjayKumarNagula@users.noreply.github.com>
2023-07-26 17:05:22 -0400

    removing APIs related to user management module

--- src/main/java/com/learn/student/userdetails/userdetailsapi/DetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.learn.student.userdetails.userdetailsapi;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface DetailsRepository extends JpaRepository<UserDetails, Integer> {
-    UserDetails findByDetails(Integer details);
-}
-
